d5abc8107206bb206431ed262900d3b9ce812ebb
Klif <sang.wicklife@student.moringaschool.com>
2024-04-14 14:35:34 +0300

    all tests passing

--- server/app.py
+++ server/app.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-
 from flask import Flask, make_response, jsonify, request, session
 from flask_migrate import Migrate
 from flask_restful import Api, Resource
@@ -21,7 +19,6 @@ api = Api(app)
 class ClearSession(Resource):
 
     def delete(self):
-    
         session['page_views'] = None
         session['user_id'] = None
 
@@ -40,7 +37,6 @@ class ShowArticle(Resource):
         session['page_views'] += 1
 
         if session['page_views'] <= 3:
-
             article = Article.query.filter(Article.id == id).first()
             article_json = jsonify(article.to_dict())
 
@@ -48,10 +44,41 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+class Login(Resource):
+
+    def post(self):
+        data = request.get_json()
+        username = data.get('username')
+        user = User.query.filter_by(username=username).first()
+
+        if user:
+            session['user_id'] = user.id
+            return {'id': user.id, 'username': user.username}, 200
+        else:
+            return {'message': 'User not found'}, 404
+
+class Logout(Resource):
+
+    def delete(self):
+        session.pop('user_id', None)
+        return {}, 204
+
+class CheckSession(Resource):
+
+    def get(self):
+        if 'user_id' in session:
+            user_id = session['user_id']
+            user = User.query.get(user_id)
+            if user:
+                return {'id': user.id, 'username': user.username}, 200
+        return {}, 401
+
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
-
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
